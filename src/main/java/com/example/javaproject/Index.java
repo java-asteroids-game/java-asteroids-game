d7e09394d5edf2c552f98cd816260a7f05c4b928
jmck90 <123461637+jmck90@users.noreply.github.com>
2023-04-23 19:20:01 +0100

    Added comments
    AsteroidType.java
    Index.java
    ProjectilyType.java

--- src/main/java/com/example/javaproject/Index.java
+++ src/main/java/com/example/javaproject/Index.java
@@ -9,9 +9,12 @@ public class Index {
 
     public static void load(Stage stage) {
         try {
+            // loads the FXML file
             Parent root = FXMLLoader.load(Index.class.getResource("index.fxml"));
+            // sets the root of the Scene of the given Stage as the loaded FXML 'root'
             stage.getScene().setRoot(root);
         }catch(IOException e) {
+            // if exception e occurs, prints stack trace
             e.printStackTrace();
         }
     }
--- src/main/java/com/example/javaproject/ProjectileType.java
+++ src/main/java/com/example/javaproject/ProjectileType.java
@@ -4,12 +4,15 @@ public enum ProjectileType {
     ALIEN("#91b37d"),
     PLAYER("#adddf0");
 
+    // a final variable, stores the String 'color' or the projectile
     private final String color;
 
+    //constructor for the ProjectileType enum
     ProjectileType(String color) {
         this.color = color;
     }
 
+    //a getter method for the color
     public String getColor() {
         return color;
     }
