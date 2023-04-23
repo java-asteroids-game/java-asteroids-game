a6df927bf524b19bfd80fc84dacef797b47c5f74
jmck90 <123461637+jmck90@users.noreply.github.com>
2023-04-23 18:39:40 +0100

    added comments
    Added comments to Asteroid.java and ControlsWindow.java

--- src/main/java/com/example/javaproject/Asteroid.java
+++ src/main/java/com/example/javaproject/Asteroid.java
@@ -12,6 +12,7 @@ public class Asteroid extends AbstractGameElement {
     public double rotationalMovement;
 
 
+    //this constructor takes x and y positions, and the AsteroidType enum as arguments, creates new Asteroid object
     public Asteroid(int x, int y, AsteroidType type) {
 
         super(new CharacterFactory().createAsteroid(type),x, y);
@@ -22,6 +23,7 @@ public class Asteroid extends AbstractGameElement {
         this.rotationalMovement = 0.5 - rnd.nextDouble();
     }
 
+    //this static method creates a list of asteroids, of size numOfAsteroids. Asteroids in random location on screen
     public static List<Asteroid> createAsteroids(int numOfAsteroids, Pane pane1) {
         List<Asteroid> asteroids = new ArrayList<>();
         for (int i = 0; i < numOfAsteroids; i++) {
@@ -33,6 +35,7 @@ public class Asteroid extends AbstractGameElement {
         return asteroids;
     }
 
+    //a method for moving an Asteroid Object
     public void move() {
         double changeX = Math.cos(Math.toRadians(this.getCharacter().getRotate()));
         double changeY = Math.sin(Math.toRadians(this.getCharacter().getRotate()));
@@ -50,16 +53,19 @@ public class Asteroid extends AbstractGameElement {
         wrapScreen();
     }
 
+    //this method increases the move speed of the Asteroid object
     public double Move_speed_up(){
         this.move_speed=0.05;
         return this.move_speed;
     }
 
+    //this method creates new asteroids on destruction, where AsteroidType is SPECIAL, LARGE or MEDIUM
     public List<Asteroid> onDestroy(){
 
         List<Asteroid> newAsteroids = new ArrayList<Asteroid>();
         AsteroidType newAsteroidType = null;
 
+        //defines newAsteroidType based on AsteroidType of destroyed Asteroid object
         switch (this.getType()) {
             case SPECIAL -> newAsteroidType = AsteroidType.LARGE;
             case LARGE -> newAsteroidType = AsteroidType.MEDIUM;
@@ -67,6 +73,7 @@ public class Asteroid extends AbstractGameElement {
             case SMALL -> {}
         }
 
+        // creates two new asteroids of the required AsteroidType
         if (newAsteroidType != null){
             for (int i = 0; i < 2; i++){
                 newAsteroids.add(new Asteroid((int) this.getCharacter().getTranslateX(), (int) this.character.getTranslateY(), newAsteroidType));
@@ -76,6 +83,7 @@ public class Asteroid extends AbstractGameElement {
         return newAsteroids;
     }
 
+    //a getter method to return AsteroidType
     public AsteroidType getType(){
         return this.type;
     }
--- src/main/java/com/example/javaproject/ControlsWindow.java
+++ src/main/java/com/example/javaproject/ControlsWindow.java
@@ -12,9 +12,11 @@ import java.util.Arrays;
 import java.util.List;
 public class ControlsWindow extends BaseGame {
 
+    //implements abstract method load() from BaseGame
+    //sets up and displays stage for the 'controls' feature
     public void load(Stage stage, int numAsteroids) {
         this.stage = stage;
-        setupGame(12);
+        setupGame(numAsteroids);
         new AnimationTimer() {
             @Override
             public void handle(long now) {
@@ -29,7 +31,7 @@ public class ControlsWindow extends BaseGame {
         stage.setResizable(false);
     }
 
-
+    //sets up user interface elements (explanation of controls) for the CoontrolsWindow
     @Override
     protected List<Text> setupUITextElements(Pane pane){
         Text titleText = new Text(((double) WIDTH /2 -60), 80, "Controls");
@@ -37,8 +39,10 @@ public class ControlsWindow extends BaseGame {
         Text controlText1 = new Text(((double) WIDTH /2 - 200), 480, "Space: Shoot    Shift: Hyperspace Jump");
         Text exitText = new Text(((double) WIDTH /2 - 120), 520, "Press ESC for Main Screen");
 
+        //an array of all text elements, iterate through the array to add text elements to pane
         List<Text> textElements = Arrays.asList(titleText, controlText, controlText1, exitText);
 
+        //iterates through all textElements, sets color, font, opacity and adds to pane
         textElements.forEach(textElement -> {
             textElement.setFill(Color.WHITE);
             textElement.setStyle("-fx-font: 20 consolas;");
@@ -51,6 +55,8 @@ public class ControlsWindow extends BaseGame {
         return textElements;
     }
 
+    // overrides/extends the populateKeyHashmap() function from the superclass
+    // the additional functionality is closing the ControlsWindow on pressing 'ESC'
     @Override
     protected void populateKeyHashMap(Scene scene){
         super.populateKeyHashMap(scene);
